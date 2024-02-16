ad56cbbf420277cde82698f03d970d6f15249f3a
zbysek.sedlacek <zbysek.sedlacek@digiteqautomotive.com>
2024-02-16 09:17:31 +0100

    removed sound :( fixed increasing

--- pushups/script.js
+++ pushups/script.js
@@ -38,7 +38,6 @@ new Vue({
         startCounter() {
             this.challengeStart = new Date();
             this._start();
-            this._playSound();
             this.save();
         },
         _start() {
@@ -59,7 +58,6 @@ new Vue({
                 if (this.currentRemainingTime <= 0) {
                     this.currentRemainingTime = intervalMilliseconds;
                     this.pushupsToDo += this.pushups;
-                    this._playSound();
                 }
 
                 if (new Date() > this.challengeEnd) {
@@ -76,12 +74,8 @@ new Vue({
             this.pushups = 10;
             this.totalTime = 7;
         },
-        _playSound(){
-            const audio = new Audio("sound.mp3");
-            audio.play();
-        },
         increasePushups(n) {
-            this.pushupsDone += n;
+            this.pushupsDone += +n;
             this.save();
         },
         formatTime(ms) {
